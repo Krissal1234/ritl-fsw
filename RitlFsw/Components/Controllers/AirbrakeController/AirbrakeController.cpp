823cc231fb2789ece0bb840b7a73d8c79bac95ac
krissal1234 <salibakris03@gmail.com>
2026-05-25 15:59:55 +0200

    added hil support

--- RitlFsw/Components/Controllers/AirbrakeController/AirbrakeController.cpp
+++ RitlFsw/Components/Controllers/AirbrakeController/AirbrakeController.cpp
@@ -5,6 +5,8 @@
 
 namespace RitlFsw {
 
+constexpr F64 AirbrakeController::INTEGRAL_CLAMP;
+
 AirbrakeController::AirbrakeController(const char* const compName)
     : AirbrakeControllerComponentBase(compName),
       m_prev_sim_time(-1.0),
